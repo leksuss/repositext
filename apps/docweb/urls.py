8755df9ec7396ba08181177f8b1284f31cae563c
Harlin Seritt <harlin.seritt@gmail.com>
2020-02-29 06:47:38 -0700

    Refactor: change root-view to repository-view

--- apps/docweb/urls.py
+++ apps/docweb/urls.py
@@ -4,6 +4,6 @@ from .views import IndexView, RepositoryView
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index-view"),
-    path('repository/folder/-ROOT-/', RepositoryView.as_view(), name="root-view"),
+    path('repository/folder/-ROOT-/', RepositoryView.as_view(), name="repository-view"),
     path('repository/folder/<uuid:folder_id>/', RepositoryView.as_view(), name="repo-view"),
 ]

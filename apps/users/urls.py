96a367f011ada180fc3e7cd45b62f5528bfd16d3
mridubhatnagar <mridubhatnagar@rediffmail.com>
2020-02-29 20:29:58 +0530

    url name removed.

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('signup/', views.SignUp.as_view()),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('home/', views.Home.as_view(), name='home'),

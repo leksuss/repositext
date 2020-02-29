1a87f75bd213e7a07ffc9594c212669e6dd3cf73
Harlin Seritt <harlin.seritt@gmail.com>
2020-02-29 06:16:31 -0700

    Add UserProfile model and to admin

--- apps/repo/admin.py
+++ apps/repo/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Document, Folder, DocumentVersion, Organization
+from .models import Document, Folder, DocumentVersion, Organization, UserProfile
 
 
 admin.site.register(Document)
 admin.site.register(Folder)
 admin.site.register(DocumentVersion)
 admin.site.register(Organization)
+admin.site.register(UserProfile)
--- apps/repo/models.py
+++ apps/repo/models.py
@@ -2,6 +2,8 @@ import uuid
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UuidPrimaryKey(models.Model):
@@ -72,3 +74,25 @@ class Organization(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    skype = models.CharField('Skype', max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()

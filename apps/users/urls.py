2a40c9ba70b641574458057649711df7a1e77eb7
mridubhatnagar <mridubhatnagar@rediffmail.com>
2020-02-29 21:01:55 +0530

    unique url name for signup added

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view()),
+    path('signup/', views.SignUp.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('home/', views.Home.as_view(), name='home'),

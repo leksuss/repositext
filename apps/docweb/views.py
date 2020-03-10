2facddd42a11b6083c197a32fb74250fd71b1f32
Harlin Seritt <harlin.seritt@gmail.com>
2020-03-09 20:11:27 -0600

    Fix code appearance

--- apps/docweb/views.py
+++ apps/docweb/views.py
@@ -18,7 +18,9 @@ def get_user_home_folder(request, home_folder):
 
 class IndexView(View):
     def get(self, request):
+        
         if request.user.username == 'admin':
+            
             root_folder = Folder.objects.get(name=SYS_ROOT_FOLDER_NAME)
             child_folders = Folder.objects.filter(parent=root_folder)
             recent_docs = Document.objects.filter().order_by(
@@ -35,21 +37,28 @@ class IndexView(View):
                 }
             )
         else:
-            return HttpResponseRedirect(reverse('user-home-view', args = [request.user.username]))
+            return HttpResponseRedirect(
+                reverse('user-home-view',
+                args=[request.user.username]
+            )
+        )
 
 
 
 class RepositoryView(View):
     def get(self, request, folder_id=None):
+        
         if folder_id:
             top_folder = Folder.objects.get(pk=folder_id)
         else:
             top_folder = Folder.objects.get(name=SYS_ROOT_FOLDER_NAME)
+        
         child_folders = Folder.objects.filter(parent=top_folder)
         child_documents = Document.objects.filter(parent=top_folder)
         add_folder_form = AddFolderForm(owner=request.user, parent=top_folder, auto_id="folder_id_%s")
         add_document_form = AddDocumentForm(owner=request.user, parent=top_folder, auto_id="document_id_%s")
         add_document_version_form = AddDocumentVersionForm()
+        
         return render(
             request,
             'docweb/repository.html',
@@ -64,20 +73,25 @@ class RepositoryView(View):
         )
 
     def post(self, request, folder_id):
+        
         parent_folder = Folder.objects.get(pk=folder_id)
         add_folder_form = AddFolderForm(
             request.POST, parent=parent_folder, owner=request.user
         )
 
         if add_folder_form.is_valid():
+            
             new_folder = add_folder_form.save(commit=False)
             new_folder.owner = request.user
             new_folder.parent = parent_folder
             new_folder.save()
             return HttpResponseRedirect(reverse('repo-view', args=[folder_id]))
+        
         else:
+            
             child_folders = Folder.objects.filter(parent=parent_folder)
             child_documents = Document.objects.filter(parent=parent_folder)
+            
             return render(
                 request,
                 'docweb/repository.html',
@@ -93,7 +107,9 @@ class RepositoryView(View):
 
 
 class UserHomeView(View):
+    
     def get(self, request, username):
+        
         root_folder = Folder.objects.get(name=SYS_ROOT_FOLDER_NAME)
         home_folder = Folder.objects.get(name='Home', parent=root_folder)
 
@@ -109,7 +125,9 @@ class UserHomeView(View):
 
 
 class AddDocumentView(View):
+    
     def post(self, request, folder_id):
+        
         parent_folder = Folder.objects.get(pk=folder_id)
         add_document_form = AddDocumentForm(request.POST, owner=request.user, parent=parent_folder)
         add_document_version_form = AddDocumentVersionForm(request.POST, request.FILES)
@@ -127,9 +145,12 @@ class AddDocumentView(View):
             doc.versions.add(dv)
             doc.save()
             return HttpResponseRedirect(reverse('repo-view', args=[parent_folder.id]))
+        
         else:
+            
             child_folders = Folder.objects.filter(parent=parent_folder)
             child_documents = Document.objects.filter(parent=parent_folder)
+            
             return render(
                 request,
                 'docweb/repository.html',
